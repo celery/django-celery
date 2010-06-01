9c99ecafa6ffe6eb1fa8c72088de92885435bbda
Vincent Driessen <vincent@datafox.nl>
2010-06-01 13:08:14 -0700

    Patch to avoid name conflict in option parser where both Django (manage.py) and celeryd use the name '--version'.
    For Django-less celeryd invocations, --version can be used. When running inside
    Django, --version is not available for 'python manage.py celeryd'.

--- djcelery/management/commands/celeryd.py
+++ djcelery/management/commands/celeryd.py
@@ -10,7 +10,11 @@ from celery.bin.celeryd import run_worker, OPTION_LIST
 
 class Command(BaseCommand):
     """Run the celery daemon."""
-    option_list = BaseCommand.option_list + OPTION_LIST
+    # Note: the filter of --version avoids a name conflict with
+    # manage.py's --version option.  Still feels a little yuck.
+    option_list = BaseCommand.option_list + \
+                  filter(lambda opt: '--version' not in opt._long_opts, \
+                         OPTION_LIST)
     help = 'Run the celery daemon'
 
     def handle(self, *args, **options):
