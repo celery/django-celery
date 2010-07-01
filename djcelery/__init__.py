28fc89a05ac9e2f41a8f54362da502842b5d2e86
Ask Solem <askh@opera.com>
2010-07-01 11:10:04 +0200

    Set homepage to "http://celeryproject.org"

--- djcelery/__init__.py
+++ djcelery/__init__.py
@@ -9,7 +9,7 @@ VERSION = (1, 1, 2)
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = "Ask Solem"
 __contact__ = "ask@celeryproject.org"
-__homepage__ = "http://github.com/ask/django-celery/"
+__homepage__ = "http://celeryproject.org"
 __docformat__ = "restructuredtext"
 
 
