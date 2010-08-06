38db59a73f650ef90e30ac895eb34dbe4daf8a27
Ask Solem <askh@opera.com>
2010-08-06 15:58:41 +0200

    setup.py: Also install distribution data files

--- setup.py
+++ setup.py
@@ -11,8 +11,60 @@ except ImportError:
     from ez_setup import use_setuptools
     use_setuptools()
     from setuptools import setup, find_packages, Command
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
 
 import djcelery as distmeta
+
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+src_dir = "djcelery"
+
+def osx_install_data(install_data):
+
+    def finalize_options(self):
+        self.set_undefined_options("install", ("install_lib", "install_dir"))
+        install_data.finalize_options(self)
+
+def fullsplit(path, result=None):
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+SKIP_EXTENSIONS = [".pyc", ".pyo", ".swp", ".swo"]
+
+
+def is_unwanted_file(filename):
+    for skip_ext in SKIP_EXTENSIONS:
+        if filename.endswith(skip_ext):
+            return True
+    return False
+
+for dirpath, dirnames, filenames in os.walk(src_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith("."):
+            del dirnames[i]
+    for filename in filenames:
+        if filename.endswith(".py"):
+            packages.append('.'.join(fullsplit(dirpath)))
+        elif is_unwanted_file(filename):
+            pass
+        else:
+            data_files.append([dirpath, [os.path.join(dirpath, f) for f in
+                filenames]])
+
 
 
 class RunTests(Command):
@@ -51,7 +103,7 @@ class QuickRunTests(RunTests):
 if os.path.exists("README.rst"):
     long_description = codecs.open("README.rst", "r", "utf-8").read()
 else:
-    long_description = "See http://pypi.python.org/pypi/django-celery"
+    long_description = "See http://github.com/ask/django-celery"
 
 
 setup(
@@ -63,12 +115,13 @@ setup(
     url=distmeta.__homepage__,
     platforms=["any"],
     license="BSD",
-    packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
+    packages=packages,
+    data_files=data_files,
     scripts=[],
     zip_safe=False,
     install_requires=[
         "django-picklefield",
-        "celery>=1.1.0",
+        "celery>=2.1.0",
     ],
     cmdclass = {"test": RunTests, "quicktest": QuickRunTests},
     classifiers=[
