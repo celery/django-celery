0d49b940fd6346a01070db5c4bebbad63a842a9e
Ask Solem <askh@opera.com>
2010-08-19 14:37:36 +0200

    get_result does not write anymore, so reason to use transaction.retry

--- djcelery/managers.py
+++ djcelery/managers.py
@@ -89,7 +89,6 @@ class PeriodicTaskManager(ExtendedManager):
 class TaskManager(ResultManager):
     """Manager for :class:`celery.models.Task` models."""
 
-    @transaction_retry(max_retries=1)
     def get_task(self, task_id):
         """Get task meta for task by ``task_id``.
 
