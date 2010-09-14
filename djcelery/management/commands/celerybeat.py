91132a6d93f225b54f257b19f0efd16c65e15438
Ask Solem <askh@opera.com>
2010-09-14 20:37:41 +0200

    Should now work with celery master branch

--- djcelery/management/commands/celerybeat.py
+++ djcelery/management/commands/celerybeat.py
@@ -3,16 +3,17 @@
 Start the celery clock service from the Django management command.
 
 """
-from celery.bin.celerybeat import run_celerybeat, OPTION_LIST
+from celery.bin import celerybeat
 
 from djcelery.management.base import CeleryCommand
+
+beat = celerybeat.BeatCommand()
 
 
 class Command(CeleryCommand):
     """Run the celery periodic task scheduler."""
-    option_list = CeleryCommand.option_list + OPTION_LIST
-    help = 'Run the celery periodic task scheduler'
+    option_list = CeleryCommand.option_list + beat.get_options()
+    help = 'Runs the Celery periodic task scheduler'
 
     def handle(self, *args, **options):
-        """Handle the management command."""
-        run_celerybeat(**options)
+        beat.run(*args, **options)
