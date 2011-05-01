6e447188fa1bfe57621f1cc2cf8071edbb6efedb
Simon Zimmermann <simonz05@gmail.com>
2011-05-01 19:35:36 +0200

    Replace function based test runner with class based. Test runners in Django 1.2 and above are a class, not a function.

--- djcelery/contrib/test_runner.py
+++ djcelery/contrib/test_runner.py
@@ -1,20 +1,21 @@
 from django.conf import settings
-from django.test.simple import run_tests as run_tests_orig
+from django.test.simple import DjangoTestSuiteRunner
 
 USAGE = """\
 Custom test runner to allow testing of celery delayed tasks.
 """
 
 
-def run_tests(test_labels, *args, **kwargs):
-    """Django test runner allowing testing of celery delayed tasks.
+class DjangoCeleryTestSuiteRunner(DjangoTestSuiteRunner):
+    def run_tests(self, test_labels, *args, **kwargs):
+        """Django test runner allowing testing of celery delayed tasks.
 
-    All tasks are run locally, not in a worker.
+        All tasks are run locally, not in a worker.
 
-    To use this runner set ``settings.TEST_RUNNER``::
+        To use this runner set ``settings.TEST_RUNNER``::
 
-        TEST_RUNNER = "celery.contrib.test_runner.run_tests"
+            TEST_RUNNER = "celery.contrib.test_runner.DjangoCeleryTestSuiteRunner"
 
-    """
-    settings.CELERY_ALWAYS_EAGER = True
-    return run_tests_orig(test_labels, *args, **kwargs)
+        """
+        settings.CELERY_ALWAYS_EAGER = True
+        return super(DjangoCeleryTestSuiteRunner, self).run_tests(test_labels, *args, **kwargs)
