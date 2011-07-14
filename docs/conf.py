f82b9c0d9910fbc8e9183a8224429ba77388d175
Ask Solem <ask@celeryproject.org>
2011-07-14 13:30:04 +0100

    Update year in LICENSE

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'django-celery'
-copyright = u'2009, Ask Solem'
+copyright = u'2009-2011, Ask Solem'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
