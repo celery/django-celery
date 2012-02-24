3d545c98153971edf5f15a08c835d61de7291f11
Ask Solem <ask@celeryproject.org>
2012-02-24 13:53:44 +0000

    AAAAAAaaaargh!!!

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,7 @@ import os
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
+sys.path.insert(0, os.getcwd())
 from django.core.management import setup_environ
 import settings
 setup_environ(settings)
