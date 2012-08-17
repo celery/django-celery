cc97b5f3bc24b87e049f21c2b00606613973301a
Ask Solem <ask@celeryproject.org>
2012-08-17 23:04:59 +0100

    Now depends on celery 3.0.6

--- setup.py
+++ setup.py
@@ -173,7 +173,7 @@ setup(
     scripts=["bin/djcelerymon"],
     zip_safe=False,
     install_requires=[
-        "celery>=3.0.4",
+        "celery>=3.0.6",
     ],
     cmdclass={"test": RunTests,
               "quicktest": QuickRunTests,
