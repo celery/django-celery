c5f8f946dc6e62535e4a5d620029b188942b9f66
Ask Solem <ask@celeryproject.org>
2015-10-09 16:11:27 -0700

    Copyright 2015

--- djcelery/__init__.py
+++ djcelery/__init__.py
@@ -1,5 +1,5 @@
 """Old django celery integration project."""
-# :copyright: (c) 2009 - 2012 by Ask Solem.
+# :copyright: (c) 2009 - 2015 by Ask Solem.
 # :license:   BSD, see LICENSE for more details.
 from __future__ import absolute_import, unicode_literals
 
