5d1ecb09c6304d22cc447c7c08fba0bd1febc2ef
蒋海波 <17843033+jianghaibo12138@users.noreply.github.com>
2019-04-24 10:24:43 +0800

    Update celery.py (#561)
    * Update celery.py
    
    fix TypeError: can only concatenate tuple (not "NoneType") to tuple #560
    
    * Update celery.py
    
    fix fix CeleryCommand has no attribute preload_options
    
    * Update setup.py
    
    update package name
    
    * Update setup.py
    
    fix package name mistake

--- djcelery/management/commands/celery.py
+++ djcelery/management/commands/celery.py
@@ -11,9 +11,16 @@ base = celery.CeleryCommand(app=app)
 class Command(CeleryCommand):
     """The celery command."""
     help = 'celery commands, see celery help'
-    options = (CeleryCommand.options +
-               base.get_options() +
-               base.preload_options)
+    cc_options = CeleryCommand.options if CeleryCommand.options else []
+    base_options = base.get_options() if base.get_options() else []
+    if hasattr(base, "preload_options"):
+        preload_options = base.preload_options if base.preload_options else []
+    else:
+        preload_options = []
+    preload_options = base.preload_options if base.preload_options else []
+    options = (cc_options +
+               base_options +
+               preload_options)
 
     def run_from_argv(self, argv):
         argv = self.handle_default_options(argv)
