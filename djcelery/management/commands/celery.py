ef5389078d223589fe06bf961b7fb3455408ba3c
ThongLe <lpthong90@gmail.com>
2019-06-03 11:18:53 +0800

    refactor get preload_options from base

--- djcelery/management/commands/celery.py
+++ djcelery/management/commands/celery.py
@@ -13,10 +13,7 @@ class Command(CeleryCommand):
     help = 'celery commands, see celery help'
     cc_options = CeleryCommand.options if CeleryCommand.options else []
     base_options = base.get_options() if base.get_options() else []
-    if hasattr(base, "preload_options"):
-        preload_options = base.preload_options if base.preload_options else []
-    else:
-        preload_options = []
+    preload_options = getattr(base, 'preload_options', []) or []
     options = (cc_options +
                base_options +
                preload_options)
